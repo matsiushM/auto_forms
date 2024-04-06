3c5fa1a043c43617c81d60fb42adf0abf21d0df3
Maxim <matiush2018@yandex.ru>
2024-04-06 12:14:05 +0300

    tests

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/auto-parts-form.netlify.app",
+  base:"/",
   plugins: [react()],
   server: {
     port: 8080,
