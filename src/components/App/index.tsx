3c6b37048a6ca1562eb657da20a00d7295ca98a1
Maxim <matiush2018@yandex.ru>
2024-04-06 12:16:10 +0300

    tests

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -15,7 +15,7 @@ const styles = {
 }
 const App = () => {
     return <Box sx={styles.paperContainer}>
-        <BrowserRouter basename='/auto-parts-form.netlify.app/'>
+        <BrowserRouter basename='/auto-parts-form.netlify.app'>
             <ButtonAppBar/>
             <Routes>
                 <Route path={'/'} element={<FormInput/>}/>
