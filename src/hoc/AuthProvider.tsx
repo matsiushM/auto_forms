e51732ce0b93a4d2207d219a061327dd475b2b82
Maxim <matiush2018@yandex.ru>
2024-07-11 05:26:01 +0300

    small fix

--- src/hoc/AuthProvider.tsx
+++ src/hoc/AuthProvider.tsx
@@ -2,6 +2,7 @@ import {createContext, useState} from "react";
 
 export const AuthContext = createContext<AuthContextType>(null);
 
+// @ts-ignore
 export const AuthProvider = ({children}) => {
     const [user, setUser] = useState("");
 
